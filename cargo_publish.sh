c8f0872a0f7eab7eb1b01ebb56ae18fbbae3b151
Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>
2022-01-31 17:05:35 +0100

    Add docs.rs CSS fix, and other minor improvements

--- cargo_publish.sh
+++ cargo_publish.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 set -euxo pipefail
-
+{% if proc_macros %}
 (cd src/proc_macros
     cargo +stable publish
 )
-
+{% endif %}
 for i in $(seq 10)
 do
     cargo +stable publish && break
     sleep 5
 done
+cargo +stable publish
